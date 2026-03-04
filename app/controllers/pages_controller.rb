bd834063bcdf0e11fc1e0f239e026807d80a3793
Romain Gateaud <romaingateaud@hotmail.com>
2026-03-04 12:58:52 +0100

    changement couleur

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!
   def home
-    @routines = current_user.routines
+    # @routines = current_user.routines
   end
 end
