b29e3c05d60974b31b060d07656ec2509400af66
Olivier Giorno <olivier.giorno@gmail.com>
2026-03-04 10:32:24 +0100

    page show routine+icone delete sidebar

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -4,27 +4,34 @@ class RoutinesController < ApplicationController
   layout "home", only: [:show]
 
   def index
-    @routines = Routine.all
     @routines = current_user.routines
   end
 
   def show
-    @routine = Routine.find(params[:id])
     @routines = current_user.routines
+    @exercices = @routine.exercice
   end
 
   def new
     @routine = Routine.new
+    @routines = current_user.routines
   end
 
   def create
-  @routine = Routine.new(routine_params)
-  @routine.user = current_user
-  if @routine.save
-    redirect_to root_path
-  else
-    render :new, status: :unprocessable_content
+    @routine = Routine.new(routine_params)
+    @routine.user = current_user
+    if @routine.save
+      redirect_to root_path
+    else
+      @routines = current_user.routines
+      render :new, status: :unprocessable_content
+    end
   end
+
+  def destroy
+    @routine = Routine.find(params[:id])
+    @routine.destroy
+    redirect_to root_path
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,29 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+#user test
+user = User.find_or_create_by!(email: "test@test.com") do |u|
+  u.password = "password"
+end
+
+#routine test
+routine = Routine.find_or_create_by!(name: "Full Body Strength", user: user)
+
+#chat test
+chat = Chat.find_or_create_by!(title: "Chat test", user: user)
+
+# exo pour routine test
+Exercice.find_or_create_by!(name: "Squat", routine: routine, chat: chat) do |e|
+  e.description = "Fléchissez les genoux jusqu'à 90 degrés, dos droit."
+  e.rep_amount = 12
+  e.video_url = "https://www.youtube.com/embed/aclHkVaku9U"
+end
+
+Exercice.find_or_create_by!(name: "Pompes", routine: routine, chat: chat) do |e|
+  e.description = "Corps gainé, descendre jusqu'au sol, pousser."
+  e.rep_amount = 15
+  e.video_url = "https://www.youtube.com/embed/IODxDxX7oi4"
+end
+
+Exercice.find_or_create_by!(name: "Fentes", routine: routine, chat: chat) do |e|
+  e.description = "Un pas en avant, genou à 90 degrés."
+  e.rep_amount = 10
+  e.video_url = "https://www.youtube.com/embed/QOVaHwm-Q6U"
+end
