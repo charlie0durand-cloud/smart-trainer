7fef064cfe2b7e87914780683f5afeeff43744dc
Olivier Giorno <olivier.giorno@gmail.com>
2026-03-06 11:34:21 +0100

    creation modale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,8 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :authenticate_user!
-  before_action :set_routines
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
 
-  private
-
-  def set_routines
-    @routines = current_user.routines
-  end
 end
